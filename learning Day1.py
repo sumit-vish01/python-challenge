5146f00a3d4658975c6e160ca1c2ede998e193c7
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-08-14 20:13:55 +0530

    first commit

---
+++ learning Day1.py
@@ -0,0 +1,220 @@
+# name = ["sumit Vishwakarma"]
+# print(name)
+
+
+#arthimatic operateres
+
+# a = 6
+# b = 7
+# print(a + b)
+# print(a - b)
+# print(a / b)
+# print(a * b)
+# print(a % b)
+# print(a // b)
+# print(a ** b)
+
+# myvar1 = 9
+# print(type(myvar1))
+
+#logical operatues
+
+# a = 7
+# b = 9
+# c = 10
+# d = 9
+# print(a == b and b == c)
+# print(a == b or b == a )
+# print(b == d or a == c)
+# print(  not a == b ,c == d)
+
+# name = input("enter your name:")
+# print("Welcome" , name)
+
+# name = input("Enter your name:")
+# print("welcome",name)
+
+# age= input("Enter your age:")
+# print("age ",age)
+
+# Date_of_birth = input("Enter your Date of birth :")
+# print("birth",Date_of_birth)
+
+# How_are_U = input("How are YOu:")
+# print("me also and thanks to asking",How_are_U)
+
+# where_are_u = input("Enter where are u studies:")
+# print("I am study from",where_are_u)
+
+# date type / variable
+
+# name = "sumit"
+# boolon = True, False
+# float = 4.53
+# integer = 4,5 
+
+
+# name = "sumit"
+# print(namr)
+
+# print("Hello, What is your namr ?")
+# name = input()
+# print("hello",name)
+
+# print("Pick up a number:")
+# num1 = input()
+# print("Pick up a second number")
+# num2 = input()
+# sum = int(num1) + int(num2)
+# print(sum)
+
+
+# print('Hello' == 'hello')
+# print(2-3+4 > 5)
+# print(4 == 4)
+# print('sumit'== 'raj')
+# print('dam' != 'tsu')
+
+# age = input("Input your age: ")
+
+# if int(age) >= 16:
+#     print("Now are ur 16")
+# else:
+#     print("You are now older than 16")
+
+
+# height = input(" enter your height:")
+
+# if int(height) <= 1:
+#     print("ur height is less than 2m")
+# elif int(height) >= 2:
+#     print("ur cannot ride , over 2m")
+# else:
+#     print("u can ride")
+
+
+# age = input("Enter your age :")
+
+# if int(age) <= 16:
+#     print("ur are not eligible to vote")
+# elif int(age) >= 20:
+#     print("Ur age are good too vote")
+# else:
+#     print("u are eligible to vote over too much")
+
+# x = 2
+# y = 4
+
+# if x == 2:
+#     if y == 3:
+#         print('x = 2 , y = 3')
+#     else:
+#         print('x =2, y != 3')
+# else:
+#     print('x != 2')
+
+# for loop
+
+# for x in range(0, 10, 1):
+# #     print(x)
+
+# for hello in range(0, 20000, 30000):
+
+#     print(hello)
+
+#while loop
+
+# loop = True
+
+# while loop:
+#     name = input("insert something")
+#     if name == 'stop':
+#       break
+
+# loop = True
+
+# while loop:
+#     password = input("Enter password:")
+#     if password == 'stop':
+#         break
+
+
+# i = 5
+# while i < 6:
+#     print(i)
+#     if i == 5:
+#         break
+#     i += 1
+
+# i = 1
+# while i < 6:
+#     i += 1
+#     if i == 3:
+#         continue
+#     print(i)
+
+# fruits = ["Banan","Papayaa","pineapple","blueberry"]
+# print(fruits[1])
+# # fruits.append('stwabeeru')
+# # print(fruits)
+
+#for loop
+
+# fruits = ['Apple','pears','strwaberrys',4, 5]
+
+# for fruit in fruits:
+#     if fruits == 'pears':
+#        print(fruits)
+# else:
+#     print('not pears')
+
+# for x in range(len(fruits)):
+#     if fruits[x] == 'pears':
+#         print(fruits[x])
+#     else:
+#         print('not a pear')
+
+# fruits = [ 'apple', 'gava','papaya']
+# for fruit in fruits:
+#     if fruits == 'gava':
+#         print( ' npt a good food')
+#     else:
+#         print('its good food')
+
+# text = input('inert something')
+# # print(text.strip())
+# # print(text.lower())
+# # print(text.upper())
+# print(text.split())
+
+
+# fruits = ['Apple','pears','strwaberrys']
+
+# print(fruits[0:])
+
+# function
+
+# def addtwo(x):
+#     return ( x + 3)
+
+# def subtracttwo(numbers):
+#     return numbers - 2
+
+# # print(addtwo(5))
+# def printstring(string):
+#     print(string)
+
+# def budget_planner(income):
+#     needs = income * 0.50
+#     wants = income * 0.30
+#     savings = income * 0.20
+#     return f" Needs: $ {needs}, wants: ${wants}, savings: ${savings}"
+
+# print(budget_planner(4000))
+
+# print(f"needs (50%): ${needs:.2f}")
+
+# file = open('file.txt', 'r')
+# f = file.readlines()
+
+# print(f)
