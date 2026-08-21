82f5d34b25d9594a490feaa27dd59b13ae8d75fd
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-08-21 19:46:34 +0530

    learnig Day 7

--- learning Day7.py
+++ learning Day7.py
@@ -1,3 +1,64 @@
-import_name.function_name()
+import math as m
 
-math.sqrt(36)
+m.squrt(36)
+from math import radians, sin, cos
+angle_degrees = 46
+angle_radians = radians(angle_degrees)
+
+sine_value = sin(angle_radians)
+cos_value = cos(angle_radians)
+
+print(sine_value)
+print(cos_value)
+
+
+from math import *
+print(sqrt(36))
+print(pow(5, 2))
+print(exp(1))
+
+import math
+print(math.pi)
+
+for i in range(1, 101):
+    print(i)
+
+for i in range(100, 0 , -1):
+    print(i)
+
+n = int(input("Enter number :"))
+
+for i in range(1, 11):
+    print(n * 1)
+
+n = 5
+
+sum = 0
+for i in range(1, n +1):
+    sum += 1
+
+print("total sum =", sum)
+
+for el in range(5):
+    print(el)
+for el in range(1 , 5):
+    print(el)
+for el in range(1, 5, 2):
+    print(el)
+
+seq = range(10)
+
+for i in seq:
+    print(i)
+
+
+veggies = ['potatoes', 'onions', 'brinjal','ladyfinger','cucumber']
+for val in veggies:
+    print(val)
+
+
+tup = (1, 2, 3,4 ,5, 6, 6,)
+
+for num in tup:
+    print(num)
+
