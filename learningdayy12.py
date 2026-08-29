47e617f3dcd8c8b65b9b82b950fa5fcd1956363f
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-08-29 08:37:39 +0530

    learningday12

---
+++ learningdayy12.py
@@ -0,0 +1,52 @@
+# from turtle import position
+
+
+# def greet(name):
+#     print("hello",name)
+
+# greet("sumit")
+
+# def greet(name="student"):
+#     print("hello",name)
+
+# greet("rahul")
+
+# def student(name,age):
+#     print(name,age)
+
+# student(age=18, name="sumit")
+
+# position arguments 
+
+# def add(a, b):
+#     print(a + b)
+
+# add(10, 20)
+
+# args 
+# def add_all(*args):
+#     total = 0
+#     for x in args:
+#         total += x
+#         return total
+
+# def add(a, b):
+#     return a + b
+
+# result = add(10, 20)
+# print(result)
+
+# def add(a, b):
+#     print(a +b)
+
+# add(100, 400)
+
+# def calculate(a, b):
+#     return a + b, a - b
+
+# x, y = calculate(10,5)
+
+# def test():
+#     x= 10
+#     print(x)
+    
