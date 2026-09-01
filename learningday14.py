604c5cfe85c983d9e4071e3da8deadc3052f7970
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-09-01 18:18:19 +0530

    try to build ourselves

---
+++ learningday14.py
@@ -0,0 +1,84 @@
+# name = "sumit"
+# age = 28
+
+# print(f'My name is {name}')
+# print(f'I am {age} years old')
+
+# name = "Sumit"
+# course = "Btech  AI / ml"
+# year = 1
+
+# print(f" My name is {name}. I am in {year}st year and studying {course}")
+
+# a = 10
+# b = 20
+
+# print(f" Sum = {a + b}")
+# print(f"multiplicaion = {a * b}")
+
+# price = 99.9999
+
+# print(f"price: ${price:.2f}")
+
+# print("hello world!", end = " ")
+# print("I will print on the same line.")
+# print("i am ", 35,"yeaers old")
+
+name = "sumit" #global vairables 
+
+# def greet():
+#     print(name)
+
+# greet()
+# print(name)
+
+# def test():
+#     age = 18
+#     print(age)
+
+# test()
+
+# def sumit():
+#     age = 39
+#     print(age)
+
+# sumit()
+
+# def test():
+#     age = 18
+
+# test()
+
+# print(age)
+# age = 10
+# def test():
+#     print(age)
+
+# test()
+
+# x = 100
+
+# def show():
+#     print(x)
+
+# show()
+
+# score = 0
+
+# def increase_score():
+#     global score
+#     score += 10
+
+# increase_score()
+# increase_score()
+# increase_score()
+
+# print(score)
+
+# x = 10
+
+# def test():
+#     print(x)
+#     x = 20
+
+# test()
