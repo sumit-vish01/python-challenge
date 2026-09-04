9f2ad0be9155a3b0476488f3757b899d47ab6feb
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-09-04 21:25:23 +0530

    learningday17.py

---
+++ learningday17.py
@@ -0,0 +1,129 @@
+# class Student:
+#     collage_name = "anbx"
+# mylist = [10,20 , 30]
+# print(mylist,type(mylist))
+# mylist = [10, 20 , 30]
+# print(mylist[len(mylist)] -1)
+
+# mylist = [10, 20, 30, "asd", 7.0, 8+8j,[1,3,4]]
+# print(mylist)
+# mylist = [10, 40, 30]
+# last = mylist.pop()
+# print(last)
+# print(mylist)
+
+# mylist = []
+
+
+# n = int(input())
+# for i in range(n):
+#     currEle = int(input())
+#     mylist.append(currEle)
+
+# print(mylist)
+# mylist = input().split(" ")
+# for i in range(len(mylist)):
+#     mylist[i] = int(mylist[i])
+
+# print(mylist, type(mylist))
+
+# mylist = input().split(" ")
+# for i in range(len(mylist)):
+#     mylist[i] = int(mylist[i])
+
+# print(mylist.type(mylist))
+
+# class Student:
+#     collage_name  = "ABC Collage"
+# def ___init___(self, name , marks):
+#     self.name = name 
+#     self.marks = marks
+#     print("Adding new student in database..")
+
+# s1 = Student("karan", 97)
+# print(s1.name,s1.marks)
+
+# class Student:
+#     Collage_name = "ABC collages"
+
+#     def __init__(self, name , marks):
+#         self.name = name
+#         self.marks = marks
+#         print("Adding new student in Database..")
+
+# s1 = Student("karan", 97)
+# print(s1.name)
+        
+# developer = 'jessica'
+# print('My name is '+ developer + ' .')
+
+# getting = 'my name is '
+# developer = 'jessica '
+
+# getting += developer
+# print(greeting)
+# my_str = "hello world "
+# print('hello'in my_str)
+
+# devloper = 'jessica'
+
+# devloper = devloper.strip()
+# print(devloper)
+
+# n = - 12
+# print(abs(n))
+# result = pow(2, 4)
+# print(result)
+
+# name = input('what is your name?')
+# print('hello',name)
+# def my_func():
+#     num = 10
+#     print(num)
+
+# tax= 0.70
+# def get_total(subtotal):
+#     total = subtotal + (subtotal * tax)
+#     return total
+
+# print(get_total(100))
+
+# age = 19
+
+# if age >= 19:
+#     print('you are an adult')
+# developer = 'Naomi'
+
+# result = developer.endswith('N') 
+# print(result)# ?
+# def greet():
+#     pass
+    
+# print(greet()) # ?
+# message = 'Python is fun!'
+
+# print(message[0:6])  # ?
+# example_list = ['example', 'dashed', 'name']
+
+# joined_str = ' '.join(example_list)
+# print(joined_str)  # ?
+# int_1 = 4
+# int_2 = 2
+
+# print(int_1 ** int_2) # ?
+# developer = 'summmmit'
+# print(list(developer))
+
+# programming_languages = ['Rust','Java','Python','C++']
+
+# for language in programming_languages:
+#     print(language)
+
+#     for char in 'code':
+# #         print(char)
+
+# categories = ['fruits','vegetables']
+# foods = ['apple','carrot','banana']
+
+# for category in categories:
+#     for foods in foods
