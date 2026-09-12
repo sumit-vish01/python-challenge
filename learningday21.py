ef46f0d6de419bf2f7bcef0d39d851294b1a1890
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-09-12 21:33:55 +0530

    try to learn

--- learningday21.py
+++ learningday21.py
@@ -36,10 +36,46 @@
 #         self.radius = value
 # my_cicle = Circle(3)
 # print('Initial radius ')
-class Dog:
-    def __init__(self, name, age):
-        self.name = name
-        self.age = age
+# class Dog:
+#     def __init__(self, name, age):
+#         self.name = name
+#         self.age = age
 
-    def bark(self):
-        print(f'{self,}')
+#     def bark(self):
+#         print(f'{self,}')
+
+# def hanoi_solver(n):
+#     rods = [
+#         list(range(n, 0, -1)),
+#         [],
+#         []
+#     ]
+
+#     moves = []
+
+#     def record_state():
+#         moves.append(f"{rods[0]} {rods[1]} {rods[2]}")
+
+#     def move_disks(num, source, target, auxiliary):
+#         if num == 0:
+#             return
+
+#         # Move num-1 disks to auxiliary rod
+#         move_disks(num - 1, source, auxiliary, target)
+
+#         # Move the top disk
+#         disk = rods[source].pop()
+#         rods[target].append(disk)
+
+#         record_state()
+
+#         # Move num-1 disks from auxiliary to target
+#         move_disks(num - 1, auxiliary, target, source)
+
+#     # Starting arrangement
+#     record_state()
+
+#     # Solve puzzle
+#     move_disks(n, 0, 2, 1)
+
+#     return "\n".join(moves)
