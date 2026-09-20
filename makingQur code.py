66917c79d52609ef8204ee530c406ed217743c67
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-09-20 23:29:33 +0530

    try to make game and try to make qr code but failed i will try soon as possible

---
+++ makingQur code.py
@@ -0,0 +1,77 @@
+# #making qurcode using python
+# import qrcode
+
+# data = "Hello, this is my first QR code!"
+
+# img = qrcode.make(data)
+
+# img.save("my_qr.png")
+
+# print("QR code successfully created!")
+
+# import qrcode
+
+# data = "https://www.google.com"
+
+# img = qrcode.make(data)
+# img.save("google_qr.png")
+
+# print("QR code created successfully!")
+
+# import qrcode
+# import qrcode.image.svg
+
+# # Data jo QR code mein daalna hai
+# data = "Hello, this is my first QR code!"
+
+# # SVG factory ka use karke bina Pillow ke QR banana
+# factory = qrcode.image.svg.SvgImage
+# img = qrcode.make(data, image_factory=factory)
+
+# # SVG format mein save karein
+# img.save("my_qr.svg")
+
+# print("QR code successfully created as my_qr.svg!")
+
+# import qrcode
+
+# data = "Hello Sumit"
+
+# img = qrcode.make(data)
+# img.save("my_qr.png")
+
+# print("QR Code created successfully!")
+
+
+# import qrcode as qr
+
+# img = qr.make("https://www.youtube.com/watch?v=FOGRHBp6lvM&list=PLjVLYmrlmjGfAUdLiF2bQ-0l8SwNZ1sBl&index=1")
+
+# img.save("wscube_youtube.png")
+
+#trying to making a game that make me helpful for student who u are  looking for that type games
+
+# n  = int(input("Enter your favourite words"))
+
+# while n >= len(5):
+#     print("You are new gamer level")
+
+
+#game 1first game 
+
+# lives = 3
+
+# while lives > 0:
+#     print("Game is running...")
+#     print("lives:", lives)
+
+#     choice = input("Do you want to contine the game")
+
+#     if choice == "no":
+#         break
+
+#     lives -= 1
+#     print("Game Over!")
+
+#game 2 making monster battle game
+
