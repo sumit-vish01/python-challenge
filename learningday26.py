b81c2a5bca103da5fc36aaf8c84274b238778b3d
sumit-vish01 <sumitvishwakarmaa950@gmail.com>
2026-09-24 22:14:38 +0530

    tyr to make somthing

--- learningday26.py
+++ learningday26.py
@@ -207,3 +207,23 @@ if age >= 21 or age >= 18 and (show_time != 'Evening' or is_member):
     print('Final price of ticket:',final_price)
 else:
     print('Ticket booking failed due to restrictions')
+
+    import random
+
+user_wins = 0
+computer_wins = 0
+
+while True:
+    user_input = input("Type Rock/paper/scissors or Q to quit: ").lower()
+    if user_input == "q":
+        break
+
+    if user_input not in ["Rock" "paper" "scissors"]:
+        continue
+
+    random_number = random.randint(0,2)
+
+
+
+print("good bye!")
+
